5ab0db913cd96196e7fba375e16787b152707f2a
Axely7 <axely7@hotmail.com>
2023-08-09 09:08:05 -0600

    buttons screen done

--- lib/presentation/screens/buttons/buttons_screen.dart
+++ lib/presentation/screens/buttons/buttons_screen.dart
@@ -55,6 +55,7 @@ class _ButtonsView extends StatelessWidget {
               onPressed: () {},
               icon: const Icon(Icons.account_box_outlined),
               label: const Text('Text Icon')),
+          const CustomButton(),
           IconButton(
               onPressed: () {},
               icon: const Icon(Icons.app_registration_rounded)),
@@ -66,6 +67,31 @@ class _ButtonsView extends StatelessWidget {
             color: Colors.white,
           )
         ]),
+      ),
+    );
+  }
+}
+
+class CustomButton extends StatelessWidget {
+  const CustomButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final colors = Theme.of(context).colorScheme;
+
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(20),
+      child: Material(
+        color: colors.primary,
+        child: InkWell(
+          onTap: () {},
+          child: const Padding(
+              padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+              child: Text(
+                'Hola mundo',
+                style: TextStyle(color: Colors.white),
+              )),
+        ),
       ),
     );
   }
