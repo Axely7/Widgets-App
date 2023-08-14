4d6149c6741f5d8c1e11b1081e997fc599f06d3c
Axely7 <axely7@hotmail.com>
2023-08-14 09:24:28 -0600

    cards finish

--- lib/presentation/screens/cards/cards_screen.dart
+++ lib/presentation/screens/cards/cards_screen.dart
@@ -35,6 +35,10 @@ class _CardsView extends StatelessWidget {
               _CardType1(label: card['label'], elevation: card['elevation'])),
           ...cards.map((card) =>
               _CardType2(label: card['label'], elevation: card['elevation'])),
+          ...cards.map((card) =>
+              _CardType3(label: card['label'], elevation: card['elevation'])),
+          ...cards.map((card) =>
+              _CardType4(label: card['label'], elevation: card['elevation'])),
           const SizedBox(
             height: 50,
           )
@@ -113,7 +117,10 @@ class _CardType3 extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final colors = Theme.of(context).colorScheme;
+
     return Card(
+      color: colors.surfaceVariant,
       elevation: elevation,
       child: Padding(
         padding: const EdgeInsets.fromLTRB(10, 5, 10, 10),
@@ -129,6 +136,40 @@ class _CardType3 extends StatelessWidget {
           )
         ]),
       ),
+    );
+  }
+}
+
+class _CardType4 extends StatelessWidget {
+  final String label;
+  final double elevation;
+
+  const _CardType4({required this.label, required this.elevation});
+
+  @override
+  Widget build(BuildContext context) {
+    final colors = Theme.of(context).colorScheme;
+
+    return Card(
+      clipBehavior: Clip.hardEdge,
+      elevation: elevation,
+      child: Stack(children: [
+        Image.network(
+          'https://picsum.photos/id/${elevation.toInt()}/600/250',
+          height: 350,
+          fit: BoxFit.cover,
+        ),
+        Align(
+            alignment: Alignment.topRight,
+            child: Container(
+              decoration: const BoxDecoration(
+                  color: Colors.white,
+                  borderRadius:
+                      BorderRadius.only(bottomLeft: Radius.circular(20))),
+              child: IconButton(
+                  onPressed: () {}, icon: const Icon(Icons.more_vert_outlined)),
+            )),
+      ]),
     );
   }
 }
