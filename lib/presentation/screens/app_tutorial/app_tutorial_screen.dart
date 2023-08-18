56aa9f681db332835e80af563584e7155f355810
Axely7 <axely7@hotmail.com>
2023-08-18 11:17:29 -0600

    app tutorial progress

--- lib/presentation/screens/app_tutorial/app_tutorial_screen.dart
+++ lib/presentation/screens/app_tutorial/app_tutorial_screen.dart
@@ -1,4 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+class SlideInfo {
+  final String title;
+  final String caption;
+  final String imageUrl;
+
+  SlideInfo(this.title, this.caption, this.imageUrl);
+}
+
+final slides = <SlideInfo>[
+  SlideInfo('Busca la comida',
+      'Quis deserunt labore officia nisi sit occaecat.', 'assets/Images/1.png'),
+  SlideInfo(
+      'Entrega rápida',
+      'Tempor aliquip elit ullamco est reprehenderit ea.',
+      'assets/Images/2.png'),
+  SlideInfo(
+      'Disfruta la comida',
+      'Nisi aute non quis quis ea voluptate eiusmod nostrud eu.',
+      'assets/Images/3.png'),
+];
 
 class AppTutorialScreen extends StatelessWidget {
   static const name = 'tutorial_screen';
@@ -7,6 +29,74 @@ class AppTutorialScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Stack(
+        children: [
+          PageView(
+            physics: const BouncingScrollPhysics(),
+            children: slides
+                .map((slideData) => _Slide(
+                    title: slideData.title,
+                    caption: slideData.caption,
+                    imageUrl: slideData.imageUrl))
+                .toList(),
+          ),
+          Positioned(
+              right: 20,
+              top: 50,
+              child: TextButton(
+                child: const Text('Salir'),
+                onPressed: () => context.pop(),
+              ))
+        ],
+      ),
+    );
+  }
+}
+
+class _Slide extends StatelessWidget {
+  final String title;
+  final String caption;
+  final String imageUrl;
+
+  const _Slide(
+      {super.key,
+      required this.title,
+      required this.caption,
+      required this.imageUrl});
+
+  @override
+  Widget build(BuildContext context) {
+    final titleStyle = Theme.of(context).textTheme.titleLarge;
+    final captionStyle = Theme.of(context).textTheme.bodySmall;
+
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 30),
+      child: Center(
+          child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Image(
+            image: AssetImage(imageUrl),
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          Text(
+            title,
+            style: titleStyle,
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          Text(
+            caption,
+            style: captionStyle,
+          )
+        ],
+      )),
+    );
   }
 }
